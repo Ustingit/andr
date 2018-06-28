7cd4d851195bc9e1245518daf43971c59af8dade
Ustinovich, Yury <y.ustinovich@a1qa.com>
2018-06-28 12:25:10 +0300

    some changes

---
+++ Application/src/androidTest/java/BaseAuthFragment_ExampleTest.java
@@ -0,0 +1,19 @@
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by y.ustinovich on 20.06.2018.
+ */
+@RunWith(AndroidJUnit4.class)
+public class BaseAuthFragment_ExampleTest {
+
+    @Test
+    public void useAppContext() throws Exception {
+        assertEquals(1, 1);
+    }
+
+}
---
+++ Application/src/test/java/BaseAuthFragment_UnitTestExample.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by y.ustinovich on 21.06.2018.
+ */
+
+public class BaseAuthFragment_UnitTestExample {
+}
